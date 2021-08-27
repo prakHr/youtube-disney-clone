1d5d476fc83a594e86bc001e7f770e305ab41b03
prakHr <gprakhar0@gmail.com>
2021-08-28 00:09:30 +0530

    final build of youtube disney clone

---
+++ secret.js
@@ -0,0 +1,23 @@
+const commands=[
+    "npx create-react-app youtube-disney-clone",
+    "cd youtube-disney-clone;npm start",
+    "cd youtube-disney-clone;npm install react-router-dom",
+    "cd youtube-disney-clone;npm i styled-components",
+    "cd youtube-disney-clone;npm i firebase@8.4.1",
+    "cd youtube-disney-clone;npm i firebase-tools@9.9.0",
+    "cd youtube-disney-clone;npm i react-redux@7.2.3",
+    "cd youtube-disney-clone;npm i @reduxjs/toolkit@1.5.1",
+    "cd youtube-disney-clone;npm i react-slick@0.28.1",
+    "cd youtube-disney-clone;npm i slick-carousel@1.8.1",
+    "cd youtube-disney-clone;firebase login",
+    "cd youtube-disney-clone;npm run build",
+    "cd youtube-disney-clone;firebase init",
+    "cd youtube-disney-clone;firebase deploy",
+    
+];
+const deployedUrl="https://disneyplus-clone-2a8de.web.app";
+
+
+const time=["50:37","2:15:45"];
+
+const url="https://www.youtube.com/watch?v=R_OERlafbmw";
